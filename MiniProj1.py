01b419fdc18eace2b956f5c14f868b9a63c35061
Dennis Brown <dennis.g.brown@gmail.com>
2021-02-28 23:57:35 -0600

    more progress...?

--- MiniProj1.py
+++ MiniProj1.py
@@ -6,6 +6,7 @@ Dennis Brown, COMP6636, 03 MAR 2021
 """
 
 import numpy as np
+import copy
 
 
 def libsvm_scale_import(filename):
@@ -28,27 +29,26 @@ def libsvm_scale_import(filename):
        
     print(num_samples, 'x', max_feature_id)
 
-    # Second pass: read data into arrays
-    y = np.zeros(num_samples)
-    X = np.zeros((num_samples, max_feature_id))
+    # Second pass: read data into array
+    X = np.zeros((num_samples, max_feature_id + 1))
     curr_sample = 0
     datafile.seek(0)
     for line in datafile:
         tokens = line.split()
-        y[curr_sample] = float(tokens[0])        
+        X[curr_sample][0] = float(tokens[0])        
         for feature in tokens[1:]:
             # print(token)
-            feature_id = int(feature.split(':')[0]) - 1
+            feature_id = int(feature.split(':')[0])
             feature_val = float(feature.split(':')[1])
             X[curr_sample][feature_id] = feature_val
         curr_sample += 1
-
     datafile.close()
-    return X, y
+
+    return X
 
 
 
-def get_neighbors(X, test_sample, num_neighbors):
+def get_neighbors(data, test_sample, num_neighbors):
     """
     Given training data, a test sample, and a number of
     neighbors, return the closest neighbors.
@@ -56,10 +56,13 @@ def get_neighbors(X, test_sample, num_neighbors):
     # Calculate all distances from the training samples
     # to this test sample. Collect index, distance
     indices_and_distances = list()
-    for i in range(len(X)):
-        indices_and_distances.append([i, np.linalg.norm(test_sample - X[i])])
+    for i in range(len(data)):
+        dist = np.linalg.norm(test_sample[1:] - (data[i])[1:]) # leave out classification at pos 0
+        indices_and_distances.append([i, dist])
+
     # Sort by distance
     indices_and_distances.sort(key=lambda _: _[1])
+
     # Make a list of requested number of closest neighbors
     neighbors = list()
     for i in range(num_neighbors):
@@ -67,29 +70,34 @@ def get_neighbors(X, test_sample, num_neighbors):
     return neighbors
  
 
-def predict_classification(X, y, test_sample, num_neighbors):
+def predict_classification(data, test_sample, num_neighbors):
     """
-    Given training data, classification data, a test sample, 
-    and a number of neighbors, predict which classification
-    the test sample belongs to.
+    Given training data, a test sample, and a number of neighbors, 
+    predict which classification the test sample belongs to.
     """
-    neighbors = get_neighbors(X, test_sample, num_neighbors)
-    output_values = list()
+    # Get closest neighbors
+    neighbors = get_neighbors(data, test_sample, num_neighbors)
+    
+    # Create list of classifications of the neighbors
+    classifications = list()
     for i in range(len(neighbors)):
-        output_values.append(y[neighbors[i]])
-    prediction = max(set(output_values), key=output_values.count)
+        classifications.append(data[neighbors[i]][0]) # 0 = classification
+    
+    # Return the most common classification of the neighbors
+    prediction = max(set(classifications), key = classifications.count)
     return prediction
 
     
-def k_nearest_neighbors(X, y, test_samples, num_neighbors):
+def k_nearest_neighbors(data, test_samples, num_neighbors):
     """
-    Given training data, classification data, test data, 
-    and a number of neighbors, predict which classification
+    Given sample data (samples are rows, columns
+    features, and samples have classifications in position 0),
+    test data, and a number of neighbors, predict which classification
     each test sample belongs to.
     """
     predictions = list()
     for i in range(len(test_samples)):
-        output = predict_classification(X, y, test_samples[i], num_neighbors)
+        output = predict_classification(data, test_samples[i], num_neighbors)
         predictions.append(output)
     return(predictions)
 
@@ -98,55 +106,65 @@ def k_nearest_neighbors(X, y, test_samples, num_neighbors):
 
 
 
-def trainPerceptron(X, y, beta, step_limit):
+def trainPerceptron(data, beta, step_limit):
     """
-    Perceptron. Given a 2-D set of data X (samples are rows, columns
-    features), a vector Y of classifications, a learning rate (beta),
-    and a step limit, train and return a weight vector that
-    can be used to classify the given data.
+    Perceptron. Given a set of data (samples are rows, columns
+    features, and samples have classifications in position 0), 
+    a learning rate (beta), and a step limit, train and return a 
+    weight vector that can be used to classify the given data.
     """
 
-    # Initialize the weight vector and add entry for bias term
-    w = np.zeros(len(X[0]) + 1)
+    # Initialize the weight vector including bias element
+    w = np.zeros(len(data[0]))
 
-    # Initialize Y_hat
-    y_hat = np.zeros((len(X), 1))
+    # Initialize y_hat
+    y_hat = np.zeros(len(data))
+
+    # Slice off y
+    y = data[:,1]
 
     # Repeat the main loop until we have convergence or reach the
     # iteration limit
     steps = 0
     converged = False
     while(not(converged) and (steps < step_limit)):
-
+        converged = True
+        
         # For each sample in X, calculate w's classification error
         # and update w.
-        for i in range(len(X)):
-            # Add a 1 to the front of every term to account for w's bias
-            sample = np.insert(X[i], 0, 1)
-            y_hat[i] = 1 if (np.matmul(w.T, sample) > 0) else -1
+        for i in range(len(data)):
+            # Replace classification in sample[0] with a 1 to allow
+            # for a biased weight vector
+            biased_sample = np.copy(data[i])
+            biased_sample[0] = 1
+
+            # Get prediction and error, then update weight vector
+            y_hat[i] = 1 if (np.matmul(w.T, biased_sample) > 0) else -1
             error = y[i] - y_hat[i]
-            w += sample * error * beta
+            w += biased_sample * error * beta
             steps += 1
 
-        # If the difference between y and y_hat is effectively 0,
-        # consider it converged.
-        if (np.linalg.norm(y - y_hat) < .0000001):
-            converged = True
+            # If error on this element is > a very small value, we have
+            # not converged.
+            if (abs(error) > 0.000001):
+                converged = False
 
     print('Final w = ', w, 'in', steps, 'steps; converged?', converged)
 
     return w
 
 
-def testWeights(X, y, w):
-    y_hat = np.zeros(len(y))
-    for i in range(len(X)):
-        sample = np.insert(X[i], 0, 1)
-        y_hat[i] = 1 if (np.matmul(w.T, sample) > 0) else -1
+def testWeights(data, w):
+    y_hat = np.zeros(len(data))
+    y = data[:,1]
+    for i in range(len(data)):
+        biased_sample = np.copy(data[i])
+        biased_sample[0] = 1
+        y_hat[i] = 1 if (np.matmul(w.T, biased_sample) > 0) else -1
     print('y   :', y)
     print('y^  :', y_hat)
     print('Diff:', y - y_hat)
-    print('sum Diff', sum(y - y_hat))
+    print('sum Diff', sum(data[:,1] - y_hat))
 
 
 
@@ -154,18 +172,24 @@ def testWeights(X, y, w):
 
 def main():
     
-    X, y = libsvm_scale_import('data/iris.scale')
-    # X, y = libsvm_scale_import('data/a4a')
-    # X, y = libsvm_scale_import('data/a4a.t')
+    data = libsvm_scale_import('data/iris.scale')
+    # data = libsvm_scale_import('data/a4a')
+    # data = libsvm_scale_import('data/a4a.t')
 
-    print(X)
+    # print(data)
+    # print('---------------')
 
 
-    blah = k_nearest_neighbors(X[:100], y[:100], X[100:], 5)
-    print(blah)
-    
-    # w = trainPerceptron(X, y, .01, 99999)
-    # testWeights(X, y, w)
+    # # Test kNN
+    # shuffleData = copy.deepcopy(data)
+    # np.random.shuffle(shuffleData)
+    # print(shuffleData[140:])    
+    # blah = k_nearest_neighbors(shuffleData[:140], shuffleData[140:], 5)
+    # print(blah)
+
+    # Test Perceptron    
+    w = trainPerceptron(data, .01, 99999)
+    testWeights(data, w)
 
 if __name__ == '__main__':
     main()
